949a7d48a7f63d26749c2b79cd1549864b1e4dd6
Idris Koken <idriskoken@hotmail.com>
2021-01-31 22:24:48 +0100

    feat: Create index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Route } from 'react-router-dom';
 import './App.css';
-import data from './data';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 function App() {
   return (
@@ -14,33 +17,9 @@ function App() {
         </div>
       </header>
       <main>
-        <div className="row center">
-          {
-            data.products.map((product) => (
-            <div key={product._id} className="card">
-            <a href={`/product/${product._id}`}>
-              <img className="medium" src={product.image} alt={product.name} />
-            </a>
-            <div className="card-body">
-            <a href={`/product/${product._id}`}>
-                <h2>{product.name}</h2>
-              </a>
-              <div className="rating">
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-                <span><i className="fa fa-star"></i></span>
-              </div>
-              <div className="price">
-                ${product.price}
-              </div>
-            </div>
-          </div>  
-            ))
-          }
-          
-        </div>
+        <Route path='/product/:id' component={ProductScreen}></Route>
+        <Route path='/' component={HomeScreen} exact></Route>
+        
       </main>
       <footer className="row center">All rights reserved</footer>
     </div>
